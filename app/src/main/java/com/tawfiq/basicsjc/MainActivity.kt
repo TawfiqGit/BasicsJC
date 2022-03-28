809564d8424f7e08f8ae8654779994578f67ac35
Tawfiq <tawfiq.mouttalif@diji.fr>
2022-03-28 17:49:23 +0200

    todo 28/03/2022

--- app/src/main/java/com/tawfiq/basicsjc/MainActivity.kt
+++ app/src/main/java/com/tawfiq/basicsjc/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.tawfiq.basicsjc
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,33 +23,71 @@ class MainActivity : ComponentActivity() {
         setContent { //Start call compose function
             BasicsJCTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val cityList= arrayListOf<String>()
+                cityList.add("India")
+                cityList.add("France")
+                cityList.add("Maroc")
+
+                AppMain(context = LocalContext.current, arrayCity =cityList )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.LightGray) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(30.dp),
-            textAlign = TextAlign.Center,
-            color = Color.Blue
-        )
+fun AppMain(context: Context,arrayCity:ArrayList<String>){
+    val ex = remember {
+        30
+    }
+    Surface( //Espace
+        color = Color.LightGray,
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxSize(1F)
+        ) {
+            for (i in 0 until arrayCity.size){
+                CityName(context = context, name = arrayCity[i])
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun CityName(context: Context,name :String){
+    Text(
+        text = "Hello $name !",
+        textAlign = TextAlign.Center,
+        color = Color.Blue
+    )
+
+    Row{
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Start")
+        }
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "Start")
+        }
+        
+        FloatingActionButton(onClick = {
+            Toast.makeText(context,"Hello Javatpoint",Toast.LENGTH_SHORT).show()
+        }) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true , heightDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsJCTheme {
-        Greeting("Android")
+        val list= arrayListOf<String>()
+        list.add("India")
+        list.add("France")
+        list.add("Maroc")
+        val context = LocalContext.current
+
+        AppMain(context = context, arrayCity = list)
     }
 }
